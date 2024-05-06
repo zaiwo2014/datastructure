167873a584163fff86b957e335b3cd0234cce68b
zaiwo2014 <1462142013@qq.com>
2024-05-06 08:11:25 +0800

    数据结构的学习

---
+++ src/main/java/com/leung/Main.java
@@ -0,0 +1,52 @@
+package com.leung;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {15, 8, 2, 4, 8, 1, 25, 1};
+        int[] ints = bubbleSort(arr);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    /**
+     * 二分查找(leftmost版本)
+     *
+     * @param arr    有序数组
+     * @param target 要查找的目标值
+     * @return
+     */
+    public static int binarySearchBasic(int[] arr, int target) {
+        //1.定义指针2
+        int i = 0;
+        int j = arr.length - 1;
+        while (i <= j) {
+            int floor = (i + j) >>> 1;
+            if (target <= arr[floor]) {
+                j = floor - 1;  //右边界缩小
+            } else if (target > arr[floor]) {
+                i = floor + 1; //左边界缩小
+            }
+        }
+        return i; //返回>= target最靠左的位置
+    }
+
+    /**
+     * 冒泡排序法
+     *
+     * @param nums
+     * @return
+     */
+    public static int[] bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
---
+++ src/main/java/com/leung/TestBinarySearch.java
@@ -0,0 +1,31 @@
+package com.leung;
+
+import java.util.Arrays;
+
+/**
+ * @title:TestBinarySearch
+ * @Author:Leung
+ * @Description:
+ * @DATE:2024/5/6 06:43
+ * @Version: 1.0
+ */
+
+public class TestBinarySearch {
+    public static void main(String[] args) {
+        int[] a = {2, 5, 8};
+        int key = 4;
+        int i = Arrays.binarySearch(a, key); //返回值为切入点
+        /**
+         * 需求:如果数组中没有要查找的key,将这个key插入到他的插入点,并返回这个新数组
+         */
+        if (i < 0) {
+            int insertIndex = Math.abs(i + 1);
+            int[] b = new int[a.length + 1];
+            System.arraycopy(a, 0, b, 0, insertIndex);
+            b[insertIndex] = key;
+            System.arraycopy(a, insertIndex, b, insertIndex + 1, a.length - insertIndex);
+            System.out.println(Arrays.toString(b));
+        }
+
+    }
+}
